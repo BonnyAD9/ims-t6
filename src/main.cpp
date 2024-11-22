341880cde83f6f604d640471b8cf68379b31f79a
Jakub Antonín Štigler <xstigl00@stud.fit.vut.cz>
2024-11-22 10:38:35 +0100

    Satisfy checks

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,42 @@
 #include <simlib.h>
 
 // global objects:
-Facility  Box("Box");
-Histogram Table("Table",0,25,20);
+static Facility box("Box");
+constexpr double TABLE_WAIT_STEP = 25;
+constexpr double TABLE_WAIT_STEP_COUNT = 20;
+static Histogram table("Table", 0, TABLE_WAIT_STEP, TABLE_WAIT_STEP_COUNT);
 
 class Customer : public Process {
-    double Prichod;                 // atribute of each customer
-    void Behavior() {               // --- behavoir specification ---
-        Prichod = Time;               // incoming time
-        Seize(Box);                   // start of service
-        Wait(10);                     // time of service
-        Release(Box);                 // end of service
-        Table(Time-Prichod);          // waiting and service time
+    static constexpr double SERVICE_TIME = 10;
+
+    double _arrival;            // atribute of each customer
+    void Behavior() override {  // --- behavoir specification ---
+        _arrival = Time;        // incoming time
+        Seize(box);             // start of service
+        Wait(SERVICE_TIME);     // time of service
+        Release(box);           // end of service
+        table(Time - _arrival); // waiting and service time
     }
 };
 
-class Generator : public Event {  // model of system's input
-    void Behavior() {               // --- behavior specification ---
-        (new Customer)->Activate();   // new customer
-        Activate(Time+Exponential(1e3/150));  //
+constexpr double MIDDLE_WAIT_TIME = 1e3 / 150;
+
+class Generator : public Event {    // model of system's input
+    void Behavior() override {      // --- behavior specification ---
+        (new Customer)->Activate(); // new customer
+        Activate(Time + Exponential(MIDDLE_WAIT_TIME)); //
     }
 };
 
-int main() {                 // experiment description
+constexpr double RUN_TIME = 1000;
+
+int main() { // experiment description
     Print(" model2 - SIMLIB/C++ example\n");
     SetOutput("model2.out");
-    Init(0,1000);              // experiment initialization for time 0..1000
+    Init(0, RUN_TIME);           // experiment initialization for time 0..1000
     (new Generator)->Activate(); // customer generator
-    Run();                     // simulation
-    Box.Output();              // print of results
-    Table.Output();
+    Run();                       // simulation
+    box.Output();                // print of results
+    table.Output();
     return 0;
 }
